551e4d065a1b8b6340694a0dfacfa102b1e00b94
dkuleschova <dkuleschova@yandex.ru>
2026-09-04 21:31:31 +0300

    Initial commit: добавлены классы Board, Cell

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoardComponent from './components/boardComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <BoardComponent />
+    </div>
+  );
+}
+
+export default App;
