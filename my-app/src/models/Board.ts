4b9fbe967f11984fab8e3590151dcf2db1dbac73
dkuleschova <dkuleschova@yandex.ru>
2026-09-08 12:41:03 +0300

    Начальная инициализация фигур

--- my-app/src/models/Board.ts
+++ my-app/src/models/Board.ts
@@ -1,5 +1,11 @@
 import {Cell} from "./Cell";
 import { Colors } from "./Colors";
+import { Bishop } from "./figures/Bishop";
+import { King } from "./figures/King";
+import { Knight } from "./figures/Knight";
+import { Pawn } from "./figures/Pawn";
+import { Queen } from "./figures/Queen";
+import { Rook } from "./figures/Rook";
 
 export class Board {
   cells: Cell[][]=[];
@@ -15,5 +21,56 @@ export class Board {
       }
       this.cells.push(row);
     }
+  }
+
+  private addPawns() {
+    for (let i=0;i<8;i++) {
+      new Pawn(Colors.WHITE, this.getCell(i, 6));
+      new Pawn(Colors.BLACK, this.getCell(i, 1));
+    }
+  }
+
+  private addBishop() {
+    new Bishop(Colors.WHITE, this.getCell(2, 7));
+    new Bishop(Colors.WHITE, this.getCell(5, 7));
+    new Bishop(Colors.BLACK, this.getCell(2, 0));
+    new Bishop(Colors.BLACK, this.getCell(5, 0));
+  }
+
+  private addKing() {
+    new King(Colors.WHITE, this.getCell(4, 7));
+    new King(Colors.BLACK, this.getCell(4, 0));
+  }
+
+  private addQueen() {
+    new Queen(Colors.WHITE, this.getCell(3, 7));
+    new Queen(Colors.BLACK, this.getCell(3, 0));
+  }
+
+  private addRook() {
+    new Rook(Colors.WHITE, this.getCell(0, 7));
+    new Rook(Colors.WHITE, this.getCell(7, 7));
+    new Rook(Colors.BLACK, this.getCell(7, 0));
+    new Rook(Colors.BLACK, this.getCell(0, 0));
+  }
+
+  private addKnight() {
+    new Knight(Colors.WHITE, this.getCell(1, 7));
+    new Knight(Colors.WHITE, this.getCell(6, 7));
+    new Knight(Colors.BLACK, this.getCell(1, 0));
+    new Knight(Colors.BLACK, this.getCell(6, 0));
+  }
+  
+  public addFigures() {
+    this.addBishop();
+    this.addKing();
+    this.addKnight();
+    this.addPawns();
+    this.addRook();
+    this.addQueen();
+  }
+
+  public getCell(x:number, y:number) {
+    return this.cells[y][x];
   }
 }
