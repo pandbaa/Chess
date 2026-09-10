89b68171fd0b9dee3b7c037228c02704be9cb48b
dkuleschova <dkuleschova@yandex.ru>
2026-09-10 23:07:36 +0300

    Логика движения фигур - Король

--- my-app/src/models/figures/King.ts
+++ my-app/src/models/figures/King.ts
@@ -12,6 +12,9 @@ export class King extends Figure {
   }
   canMove(target: Cell): boolean {
     if (!super.canMove(target)) return false;
-    return true;
+    const dx=Math.abs(this.cell.x-target.x);
+    const dy=Math.abs(this.cell.y-target.y);
+    if (dx<=1 && dy<=1 && dx + dy > 0) return true;
+    return false;
   }
 }
